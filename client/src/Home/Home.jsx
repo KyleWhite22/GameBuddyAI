b3cf9afc5295f63be908500d5fdd690556e2c69c
KyleWhite22 <kewhite900@gmail.com>
2025-05-29 20:40:06 -0400

    added home page

---
+++ client/src/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home-container">
+                <h1>GameTracker AI</h1>
+            <p>Your personal video game journal and AI assistant.</p>
+        </div>
+    );
+        
+}
+
+export default Home;
